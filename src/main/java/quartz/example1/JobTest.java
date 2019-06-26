315c76bc38f2549dad0f502a025d36472f1742f3
changhao <www.changhao@outlook.com>
2019-06-26 22:30:57 +0800

    JobDataMap Demo

--- src/main/java/quartz/example1/JobTest.java
+++ src/main/java/quartz/example1/JobTest.java
@@ -18,12 +18,15 @@ import static org.quartz.TriggerBuilder.newTrigger;
 public class JobTest {
     public static void main(String[] args) {
         try {
+            //获取调度器
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
 
+            //定义Job信息
             JobDetail detail = newJob(HelloJob.class)
                     .withIdentity("hello", "group1")
                     .build();
 
+            //定义触发器
             Trigger trigger = newTrigger()
                     .withIdentity("trigger1", "group1")
                     .startNow()
@@ -31,8 +34,10 @@ public class JobTest {
                             .withIntervalInSeconds(10)
                             .withRepeatCount(10))
                     .build();
-            scheduler.scheduleJob(detail, trigger);
 
+            //添加Job到调度器中
+            scheduler.scheduleJob(detail, trigger);
+            //启动调度器
             scheduler.start();
             //scheduler.shutdown(true);
         } catch (SchedulerException e) {
---
+++ src/main/java/quartz/example2/QuartzInjectTest.java
@@ -0,0 +1,66 @@
+package quartz.example2;
+
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+
+/**
+ * Quartz 使用JobData自动注入
+ *
+ * @author LDDFY
+ */
+public class QuartzInjectTest {
+
+    @Slf4j
+    public static class QuartzJob implements Job {
+        public static final String KEY = "id";
+        public static final String SCORE = "score";
+
+        @Setter
+        private String id;
+        @Setter
+        private float score;
+
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            JobKey jobKey = context.getJobDetail().getKey();
+            log.info("Instance:{},id:{},score:{}", jobKey, id, score);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            JobDetail jobDetail = newJob(QuartzJob.class)
+                    .withIdentity("myJob", "myGroup")
+                    .usingJobData(QuartzJob.KEY, "jobKey")
+                    .usingJobData(QuartzJob.SCORE, 1.23f)
+                    .build();
+
+            Trigger trigger = TriggerBuilder
+                    .newTrigger()
+                    .withIdentity("myTrigger", "myGroup")
+                    .startNow()
+                    .withSchedule(simpleSchedule()
+                            .withIntervalInSeconds(3)
+                            .withRepeatCount(10))
+                    .build();
+
+            scheduler.scheduleJob(jobDetail, trigger);
+            scheduler.start();
+            TimeUnit.SECONDS.sleep(32);
+            scheduler.shutdown(true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
+    }
+}
